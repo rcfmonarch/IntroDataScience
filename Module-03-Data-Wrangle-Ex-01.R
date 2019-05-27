7b06458dc0c27d3c11523e75961d3b8a15ee97f8
Rob Fontenot <rcfmonarch@gmail.com>
2019-05-27 14:24:05 -0400

    Mod-3-Data-Wrangle-Ex1
    Completed files for Mod-3 Data Wrangling Exercise 1

---
+++ Module-03-Data-Wrangle-Ex-01.R
@@ -0,0 +1,75 @@
+#--------------------------------------------------------
+# Module 3 Data Wrangling Ex-01
+# Rob Fontenot
+# rcfmonarch@gmail.com
+# https://github.com/rcfmonarch/IntroDataScience
+# 2019-05-27
+#--------------------------------------------------------
+
+library(dplyr)
+library(tidyr)
+
+# Step 0: Load data set into RStudio
+data <- read.csv("D:\\Hermetic-Code\\R\\IntroDataScience\\
+                 Module-03-Data-Wrangle-Ex-01-Refine-Original.csv")
+data
+
+# Step 1: Clean up Brand Names
+data$companyfix <- sapply(data$ï..company, tolower)
+
+for(i in 1:length(data$companyfix)){
+  if (data$companyfix[i] == "ak zo" 
+      | data$companyfix[i] == "akz0") {
+    data$companyfix[i] = "akzo"
+  }
+  if (data$companyfix[i] == "phillips" 
+      | data$companyfix[i] == "fillips"
+      | data$companyfix[i] == "phllips"
+      | data$companyfix[i] == "phillps"
+      | data$companyfix[i] == "phlips") {
+    data$companyfix[i] = "philips"
+  }
+  if (data$companyfix[i] == "unilver") {
+    data$companyfix[i] = "unilever"
+  }
+}
+data$companyfix
+
+# Step 2:  Separate Product Code and Number
+datafix <- data %>% 
+  separate(Product.code...number, c("product_code", "product_number"), remove=FALSE)
+datafix
+
+# Step 3: Add Product Categories
+
+datafix2 <- datafix %>% 
+  mutate("product_category" = case_when (
+    datafix$product_code == "p" ~ "Smartphone",
+    datafix$product_code == "q" ~ "Tablet",
+    datafix$product_code == "v" ~ "TV", 
+    datafix$product_code == "x" ~ "Laptop",
+    TRUE ~ datafix$product_code
+    ))
+datafix2
+
+# Step 4: Add full address for geocoding
+
+datafix3 <- datafix2 %>% 
+  mutate("full_address" = paste(address, city, country, sep=","))
+datafix3
+
+# Step 5: Create Dummary Variables for Company and Product Category
+
+datafix4 <- datafix3 %>% 
+  mutate("company_philips" = ifelse(companyfix == "philips",1,0)) %>% 
+  mutate("company_akzo" = ifelse(companyfix == "akzo",1,0)) %>% 
+  mutate("company_van_houten" = ifelse(companyfix == "van houten",1,0)) %>% 
+  mutate("company_unilever" = ifelse(companyfix == "unilever",1,0)) %>% 
+  mutate("product_smartphone" = ifelse(product_category == "Smartphone",1,0)) %>% 
+  mutate("product_tv" = ifelse(product_category == "TV",1,0)) %>% 
+  mutate("product_laptop" = ifelse(product_category == "Laptop",1,0)) %>% 
+  mutate("product_tablet" = ifelse(product_category == "Tablet",1,0))
+  
+datafix4
+
+write.csv(datafix4, file="Module-03-Data-Wrangle-Ex-01-Refine-Clean.csv", row.names=FALSE)
---
+++ Module-03-dplyr-practice.R
@@ -0,0 +1,94 @@
+library(dplyr)
+library(hflights)
+
+data(hdflights)
+head(hflights)
+
+flights <- tbl_df(hflights)
+flights
+print(flights, n=20)
+
+data.frame(head(flights))
+
+flights[flights$Month==1 & flights$DayofMonth==1, ]
+filter(flights, Month==1, DayofMonth==1)
+filter(flights, UniqueCarrier=="AA" | UniqueCarrier=="UA")
+
+flights[ , c("DepTime", "ArrTime", "FlightNum")]
+select(flights, DepTime, ArrTime, FlightNum)
+
+flights %>% 
+  select(UniqueCarrier, DepDelay) %>% 
+  filter(DepDelay > 60)
+
+
+x1 <- 1:5; x2 <- 2:6
+sqrt(sum((x1-x2)^2))
+(x1-x2)^2 %>%  sum() %>% sqrt()
+
+flights$Speed <- flights$Distance/flights$AirTime*60
+flights[, c("Distance", "AirTime", "Speed")]
+
+flights %>% 
+  select(Distance, AirTime) %>% 
+  mutate(Speed = Distance/AirTime*60)
+
+flights <- flights %>% mutate(Speed = Distance/AirTime*60)
+flights
+
+flights %>% 
+  group_by(Dest) %>% 
+  summarise(avg_delay = mean(ArrDelay, na.rm=TRUE))
+
+flights %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise_each(funs(mean), Cancelled, Diverted)
+
+flights %>% 
+  group_by(UniqueCarrier) %>% 
+  summarise_each(funs(min(., na.rm=TRUE), max(., na.rm=TRUE)), matches("Delay"))
+
+flights %>% 
+  group_by(Month, DayofMonth) %>% 
+  summarise(flight_count = n()) %>% 
+  arrange(desc(flight_count))
+
+flights %>% 
+  group_by(Month, DayofMonth) %>% 
+  tally(sort = TRUE)
+
+flights %>% 
+  group_by(Dest) %>% 
+  summarise(flight_count = n(), plane_count = n_distinct(TailNum))
+
+flights %>% 
+  group_by(Dest) %>% 
+  select(Cancelled) %>% 
+  table() %>% 
+  head()
+
+flights %>% 
+  group_by(UniqueCarrier) %>% 
+  select(Month, DayofMonth, DepDelay) %>% 
+  filter(min_rank(desc(DepDelay)) <= 2) %>% 
+  arrange(UniqueCarrier, desc(DepDelay))
+
+flights %>% 
+  group_by(UniqueCarrier) %>% 
+  select(Month, DayofMonth, DepDelay) %>% 
+  top_n(2) %>% 
+  arrange(UniqueCarrier, desc(DepDelay))
+
+flights %>% 
+  group_by(Month) %>% 
+  summarise(flight_count = n()) %>% 
+  mutate(change = flight_count - lag(flight_count))
+
+glimpse(flights)
+
+
+
+
+
+
+
